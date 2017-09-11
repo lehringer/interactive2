7bd92f81bdaf01290bd267518abdf5130e29402c
lehringer <logibear56@MacBook-Pro-6.local>
2017-09-11 12:16:48 -0700

    interactive2

---
+++ exercise-charrette/assets/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+	// $('#button-2').onClick(function() {
+	// $('.container').animate({
+	// 		backgroundColor="black"
+		
+	// 	}, 1000 );
+	// });
+	
+// 	$('#button-4').click(function() {
+// 		$('#button-3').animate({
+// 			opacity: 0.4,
+// 			marginLeft: '0.6in',
+// 			fontSize: '24px',
+// 			borderWidth: '10px'
+		
+// 		}, 1000 );
+// 	});
+
+// 	$('#button-4').mouseover(function() {
+// 		$('.item').animate({
+// 			marginLeft: '1in',
+		
+// 		}, 1000 );
+// 	});
+// });
