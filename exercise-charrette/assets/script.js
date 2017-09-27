dd336d0d686b7ac370606fef07a2f4b134fffdfe
lehringer <logibear56@MacBook-Pro-7.local>
2017-09-27 13:05:27 -0700

    as

--- exercise-charrette/assets/script.js
+++ exercise-charrette/assets/script.js
@@ -7,20 +7,45 @@ $(document).ready(function(){
 	// 	}, 1000 );
 	// });
 	
-// 	$('#button-4').click(function() {
-// 		$('#button-3').animate({
-// 			opacity: 0.4,
-// 			marginLeft: '0.6in',
-// 			fontSize: '24px',
-// 			borderWidth: '10px'
+	// $('#button-4').click(function() {
+	// 	$('#button-3').animate({
+	// 		opacity: 0.4,
+	// 		marginLeft: '0.6in',
+	// 		fontSize: '24px',
+	// 		borderWidth: '10px'
 		
-// 		}, 1000 );
-// 	});
+	// 	}, 1000 );
+	// });
 
-// 	$('#button-4').mouseover(function() {
-// 		$('.item').animate({
-// 			marginLeft: '1in',
+	// $('#button-4').mouseover(function() {
+	// 	$('.item').animate({
+	// 		marginLeft: '1in',
 		
-// 		}, 1000 );
-// 	});
-// });
+	// 	}, 1000 );
+	// });
+
+	// 
+    $("#button-13").draggable();
+  });
+
+		$('#button-34').click(function openWin() {
+    myWindow = window.open("http://www.starwars.com/", "_blank", "width=600, height=450");
+});
+var myWindow;
+	$('#button-40').click(function openWin() {
+    myWindow = window.open("", "myWindow", "width=300,height=300");
+    myWindow.document.write("<p>hehe psych</p>");
+});
+		$('#button-41').click(function closeWin() {
+    myWindow.close();
+});
+		$( function() {
+    $( "#button-47" ).draggable();
+  } );
+
+        var counter = $('#TextBox').val();
+        $('#AddButton').click( function() {
+            var counter = $('#TextBox').val();
+            counter++ ;
+            $('#TextBox').val(counter);
+    });
---
+++ minisite/script.js
@@ -0,0 +1,23 @@
+$( "#red" ).onmouseover()
+  element.classlist.remove("box1");
+// $( ".box1" ).animate({
+
+    // Animation complete.
+
+
+
+$( "#red" ).onmouseout(function() {
+$( ".box1" ).animate({
+  }, 3000, function() {
+    // Animation complete.
+  });
+});
+
+// "use strict";
+
+// var element = document.getElementById("red")
+// element.addEventListener("click", function(e){
+//   e.preventDefault;
+//   element.classlist.remove("box1")
+//   void element.offsetWidth;
+//   element.classList.add("box1");}, false);
